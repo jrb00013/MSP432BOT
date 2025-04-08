e2a50aa687fda9b6dd8352d6c78f4193834fad36
Joe Black <156119500+jrb00013@users.noreply.github.com>
2025-04-08 09:42:36 -0400

    init_uart prototype added

--- include/uart.h
+++ include/uart.h
@@ -1,7 +1,8 @@
-#ifndef SENSORS_H
-#define SENSORS_H
+#ifndef UART_H
+#define UART_H
 
-void init_sensors(void);
-int read_sensors(void);
+void init_uart(void);
+void uart_send_char(char c);
+char uart_receive_char(void);
 
-#endif // SENSORS_H
+#endif // UART_H
