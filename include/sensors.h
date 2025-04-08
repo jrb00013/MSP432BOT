2bb66f1e8148dfecaa2515629991f880c6b562fa
Joe Black <156119500+jrb00013@users.noreply.github.com>
2025-04-08 09:48:49 -0400

    sensor function prototypes updated

--- include/sensors.h
+++ include/sensors.h
@@ -1,7 +1,10 @@
 #ifndef SENSORS_H
 #define SENSORS_H
 
+// Function prototypes for sensor initialization and reading
 void init_sensors(void);
-int read_sensors(void);
+int read_ultrasonic_sensor(void);
+int read_infrared_sensor(void);
+int read_temperature_sensor(void);
 
 #endif // SENSORS_H
