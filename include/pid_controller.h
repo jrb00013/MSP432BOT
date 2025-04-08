b23ef23b11ce76ea2faa6ddb974bfc646662ae39
Joe Black <156119500+jrb00013@users.noreply.github.com>
2025-04-08 09:47:29 -0400

    pid control struct updated

--- include/pid_controller.h
+++ include/pid_controller.h
@@ -1,6 +1,16 @@
 #ifndef PID_CONTROLLER_H
 #define PID_CONTROLLER_H
 
-int pid_control(int sensor_value);
+// Define a PID control structure
+typedef struct {
+    float kp;
+    float ki;
+    float kd;
+    float previous_error;
+    float integral;
+    float control_signal;
+} PID;
+
+int pid_control(PID* pid, int sensor_value, int setpoint);
 
 #endif // PID_CONTROLLER_H
